507f54fcd33c30cda696bb97e859b1f3935b1f91
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-03 19:37:25 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6, }+\z/i
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX}
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+  
 end
