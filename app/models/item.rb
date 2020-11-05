e9cc3ee2dc8513cb58af67c86825b80ea1faac82
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-05 18:59:24 +0900

    バリデーションの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,19 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :content
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :delivery_fee_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :ship_day_id, numericality: { other_than: 1 }
     validates :price,
-                    format: { with: greater_than_or_equal_to: 300 },
-                    format: { with: less_than_or_equal_to: 9,999,999 },
+                    numericality: { greater_than_or_equal_to: 300 },
+                    numericality: { less_than_or_equal_to: 9999999 },
                     format: { with: /\A[0-9]+\z/ }
     validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :ship_day_id
   end
 
   belongs_to :user
