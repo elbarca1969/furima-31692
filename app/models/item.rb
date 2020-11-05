2cc0fc08cd169da78f56a8e18da52cbd2365e5fb
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-05 16:55:40 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ApplicationRecord
 
+  with_options presence: true do
+    validates :item_name
+    validates :content
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :ship_day_id, numericality: { other_than: 1 }
+    validates :price,
+                    format: { with: greater_than_or_equal_to: 300 },
+                    format: { with: less_than_or_equal_to: 9,999,999 },
+                    format: { with: /\A[0-9]+\z/ }
+    validates :image
+  end
+
   belongs_to :user
   has_one_attached :image
 
@@ -9,5 +24,5 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :ship_day
-  
+
 end
