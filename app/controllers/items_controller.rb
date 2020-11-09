40df949d69060bf03f2f74c7aec1a34466b64140
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-09 17:12:51 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
   
   def index
     @items = Item.order("created_at DESC")
@@ -39,6 +40,12 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :ship_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user_id
+      redirect_to root_path
+    end
   end
 
 end
