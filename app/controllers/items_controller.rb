b8e0af03f677ba9e10d5d99a3064beed91021492
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-09 18:55:31 +0900

    destroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to action: :index
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :ship_day_id, :price, :image).merge(user_id: current_user.id)
