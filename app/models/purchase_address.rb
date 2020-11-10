95cba349e65584eea66bead43ccb65021bbc45c8
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-10 17:50:22 +0900

    Formオブジェクトの編集

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -12,7 +12,7 @@ class PurchaseAddress
   validates :prefecture_id, numericality: { other_than: 1 }
   
   def save
-    Purchase.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
