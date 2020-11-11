57725060dc6cc43757405d89f2e3e9d3e8bcb8d7
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-11 18:17:52 +0900

    ユーザーに合わせたパスの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,10 @@
 class PurchasesController < ApplicationController
-  
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: :index
+
   def index
     @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -28,6 +31,13 @@ class PurchasesController < ApplicationController
       card: purchase_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
 end
